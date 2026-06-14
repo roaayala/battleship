5ac0bc0a18f63990a9fec07e0d7e7d5b77d295f9
roaayala <roaayala.ahm@gmail.com>
2026-06-14 21:02:21 +0800

    models: Gameboard WIP

---
+++ src/models/Gameboard.js
@@ -0,0 +1,9 @@
+export default function createGameboard() {
+  let missedAttack = [];
+
+  const missedAttackRecord = () => missedAttack;
+
+  const placeShip = (x, y) => {};
+
+  const receiveAttack = (x, y) => {};
+}
---
+++ src/tests/Gameboard.test.js
@@ -0,0 +1,1 @@
+import createGameboard from "../models/Gameboard";
