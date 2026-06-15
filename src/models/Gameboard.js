c7f022ba6565611435bfff4284bc299997f020bb
roaayala <roaayala.ahm@gmail.com>
2026-06-15 20:38:42 +0800

    models: Gameboard - placeShip

--- src/models/Gameboard.js
+++ src/models/Gameboard.js
@@ -1,9 +1,31 @@
+import createShip from "./Ship";
+
 export default function createGameboard() {
-  let missedAttack = [];
+  const boardData = Array(10)
+    .fill(null)
+    .map(() => Array(10).fill(null));
+
+  let missedAttack = 0;
 
   const missedAttackRecord = () => missedAttack;
 
-  const placeShip = (x, y) => {};
+  const placeShip = (length, startX, startY, isVertical = false) => {
+    const newShip = createShip(length);
+
+    if (isVertical) {
+      // vertical placement
+      for (let i = 0; i < length; i++) {
+        boardData[startY + i][startX] = newShip;
+      }
+    } else {
+      // horizontal placement
+      for (let i = 0; i < length; i++) {
+        boardData[startY][startX + i] = newShip;
+      }
+    }
+  };
 
   const receiveAttack = (x, y) => {};
+
+  return { getBoard: () => boardData, placeShip };
 }
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,1 +1,60 @@
 import createGameboard from "../models/Gameboard";
+
+test("2 dimension array 10x10", () => {
+  const gameboard = createGameboard();
+
+  expect(gameboard.getBoard()).toStrictEqual([
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+  ]);
+});
+
+test("vertical ship placement", () => {
+  const gameboard = createGameboard();
+
+  gameboard.placeShip(2, 1, 0, true);
+
+  const board = gameboard.getBoard();
+
+  expect(board[0][1]).not.toBeNull();
+
+  expect(board[0][1].length).toBe(2);
+
+  expect(board[1][1]).not.toBeNull();
+
+  // same ship
+  expect(board[1][1]).toBe(board[0][1]);
+
+  // check other tiles must be null
+  expect(board[0][0]).toBeNull();
+  expect(board[2][1]).toBeNull();
+});
+
+test("horizontal ship placement", () => {
+  const gameboard = createGameboard();
+
+  gameboard.placeShip(2, 1, 0, false);
+
+  const board = gameboard.getBoard();
+
+  expect(board[0][1]).not.toBeNull();
+
+  expect(board[0][2].length).toBe(2);
+
+  expect(board[0][1]).not.toBeNull();
+
+  // same ship
+  expect(board[0][1]).toBe(board[0][2]);
+
+  // check other tiles must be null
+  expect(board[0][0]).toBeNull();
+  expect(board[0][3]).toBeNull();
+});
