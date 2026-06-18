6e40f2f0f6e79f127e85dac4c047478ff9e338ca
roaayala <roaayala.ahm@gmail.com>
2026-06-18 15:02:46 +0800

    models/Gameboard: out of bounds and overlap guard clauses

--- src/models/Gameboard.js
+++ src/models/Gameboard.js
@@ -9,23 +9,38 @@ export default function createGameboard() {
   let shipsOnBoard = [];
 
   const placeShip = ({ shipLength, xAxis, yAxis, isVertical = false }) => {
+    // out of bounds
+    if (xAxis < 0 || xAxis > 9 || yAxis < 0 || yAxis > 9) {
+      return false;
+    }
+
+    // overlap
+
     const newShip = createShip(shipLength);
 
     shipsOnBoard = [...shipsOnBoard, newShip];
 
-
-    const updatedBoard = board.map((row, y) => {
+    board = board.map((row, y) => {
       return row.map((cell, x) => {
-        const shipPath = isVertical ? x === xAxis:;
-        })
+        // vertical: lock x, and y must greater than or equal yAxis, and y should less than yAxis plus  shipLength
+
+        // horizontal: lock y, and x must greather than or equal of xAxis, and x should less than xAxis + shiplength
+        const shipPath = isVertical
+          ? // lock x, put ship in static x on dynamic y
+            x === xAxis && y >= yAxis && y < yAxis + shipLength
+          : // lock y, put ship in dynamic x on static y
+            y === yAxis && x >= xAxis && x < xAxis + shipLength;
+
+        return shipPath ? newShip : cell;
       });
+    });
 
-    board = updatedBoard;
-
+    return true;
   };
 
   return {
     getBoard: () => board,
     getMissedAttacks: () => missedAttackRecord,
+    placeShip,
   };
 }
