e5a4fe45999728f719738e473d15d051f8d7a74c
roaayala <roaayala.ahm@gmail.com>
2026-06-19 23:07:47 +0800

    Gameboard and Gameboard.test: .randomizeAttack

--- src/models/Player.js
+++ src/models/Player.js
@@ -4,10 +4,28 @@ export default function createPlayer({ name, isHuman = true }) {
   const board = createGameboard();
 
   // for non human player
-  let availableMoves = [];
+  const availableMoves = [];
 
   if (!isHuman) {
+    for (let y = 0; y < board.getBoard().length; y++) {
+      for (let x = 0; x < board.getBoard()[y].length; x++) {
+        availableMoves.push([x, y]);
+      }
+    }
   }
 
-  return { name, isHuman, getGameboard: () => board };
+  const randomizeAttack = (enemyGameboard) => {
+    const rIndex = Math.floor(Math.random() * availableMoves.length);
+
+    const [x, y] = availableMoves.splice(rIndex, 1)[0];
+
+    enemyGameboard.receiveAttack(x, y);
+  };
+
+  return {
+    name,
+    isHuman,
+    getGameboard: () => board,
+    randomizeAttack,
+  };
 }
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,3 +1,4 @@
+import createGameboard from "../models/Gameboard";
 import createPlayer from "../models/Player";
 
 test("player props", () => {
@@ -7,4 +8,14 @@ test("player props", () => {
   expect(p1.name).toBe("Player One");
   expect(p1.isHuman).toBe(true);
   expect(p1.getGameboard().getMissedAttacks().length).toBe(0);
+});
+
+test("randomizeAttack", () => {
+  const p2 = createPlayer({ name: "Player Two", isHuman: false });
+  const computerTarget = createGameboard();
+  p2.randomizeAttack(computerTarget);
+  expect(computerTarget.getMissedAttacks().length).toBe(1);
+
+  const p1 = createPlayer({ name: "Player One", isHuman: true });
+  const humanTarget = createGameboard();
 });
