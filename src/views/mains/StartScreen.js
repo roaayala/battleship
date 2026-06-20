c23ceadfeb894160cc72a049b8e20179cce3e428
roaayala <roaayala.ahm@gmail.com>
2026-06-20 13:07:22 +0800

    views/mains/StartScreen: WIP

--- src/views/mains/StartScreen.js
+++ src/views/mains/StartScreen.js
@@ -8,6 +8,10 @@ export default function createStartScreen() {
   startScreenTitle.className = "start-screen__title";
   startScreenTitle.textContent = "Player Setup";
 
+  const startScreenMain = document.createElement("main");
+
+  const startScreenFooter = document.createElement("footer");
+
   const arrangeShipButton = createButton({
     text: "Arrange Ship",
     fn: () => {
@@ -15,6 +19,8 @@ export default function createStartScreen() {
     },
   });
 
-  startScreen.append(startScreenTitle, arrangeShipButton);
+  startScreenFooter.append(arrangeShipButton);
+
+  startScreen.append(startScreenTitle, startScreenMain, startScreenFooter);
   return startScreen;
 }
