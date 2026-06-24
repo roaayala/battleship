3f59d960f56e6f6adb04b42bb23e187acf062020
roaayala <roaayala.ahm@gmail.com>
2026-06-24 21:51:11 +0800

    hunt and target for computer is done

--- src/models/Player.js
+++ src/models/Player.js
@@ -5,6 +5,7 @@ export default function createPlayer({ name, isHuman = true }) {
 
   // for non human player
   const availableMoves = [];
+  const recommendedMoves = [];
 
   if (!isHuman) {
     for (let y = 0; y < board.getBoard().length; y++) {
@@ -15,11 +16,67 @@ export default function createPlayer({ name, isHuman = true }) {
   }
 
   const randomizeAttack = (enemyGameboard) => {
-    const rIndex = Math.floor(Math.random() * availableMoves.length);
+    const getValidCoordinate = (cx, cy) => {
+      const potentialMoves = [];
 
-    const [x, y] = availableMoves.splice(rIndex, 1)[0];
+      if (cx > 0) potentialMoves.push([cx - 1, cy]);
+      if (cx < 9) potentialMoves.push([cx + 1, cy]);
+      if (cy > 0) potentialMoves.push([cx, cy - 1]);
+      if (cy < 9) potentialMoves.push([cx, cy + 1]);
 
-    enemyGameboard.receiveAttack(x, y);
+      // only return not attacked coordinate
+      return potentialMoves.filter(([px, py]) => {
+        return availableMoves.some(([ax, ay]) => {
+          if (ax === px && ay === py) {
+            return true;
+          }
+          return false;
+        });
+      });
+    };
+
+    let currentX, currentY;
+
+    if (recommendedMoves.length > 0) {
+      // hunt
+      const rIndex = Math.floor(Math.random() * recommendedMoves.length);
+
+      const [hx, hy] = recommendedMoves.splice(rIndex, 1)[0];
+      currentX = hx;
+      currentY = hy;
+
+      // remove coordinate from availableMoves when coordinate from recommendedMoves used
+      const compIndex = availableMoves.findIndex(
+        ([ciX, ciY]) => ciX === currentX && ciY === currentY,
+      );
+
+      if (compIndex !== -1) availableMoves.splice(compIndex, 1);
+    } else {
+      // target
+      const rIndex = Math.floor(Math.random() * availableMoves.length);
+
+      const [x, y] = availableMoves.splice(rIndex, 1)[0];
+      currentX = x;
+      currentY = y;
+    }
+
+    const attackResult = enemyGameboard.receiveAttack(currentX, currentY);
+
+    if (attackResult.isHit) {
+      const generateRecommendations = getValidCoordinate(currentX, currentY);
+
+      generateRecommendations.forEach((newRec) => {
+        const isAlreadyRecommended = recommendedMoves.some(
+          (rx, ry) => rx === newRec[0] && ry === newRec[1],
+        );
+
+        if (!isAlreadyRecommended) {
+          recommendedMoves.push(newRec);
+        }
+      });
+    }
+
+    return attackResult;
   };
 
   // for human player
